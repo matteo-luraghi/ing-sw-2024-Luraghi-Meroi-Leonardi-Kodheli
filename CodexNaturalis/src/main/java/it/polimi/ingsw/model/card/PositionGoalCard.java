689d358510390f97285e93cd3407d3f35e066d6d
lorenzoMeroi22 <lorenzo.meroi@mail.polimi.it>
2024-03-23 10:55:09 +0100

    GoalCards finished, CARD package ended

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/PositionGoalCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/PositionGoalCard.java
@@ -1,4 +1,64 @@
 package it.polimi.ingsw.model.card;
 
+import java.util.ArrayList;
+
+/**
+ * class PositionGoalCard
+ * @author Lorenzo Meroi
+ */
 public class PositionGoalCard extends GoalCard{
+    private int Points;
+
+    private boolean isResourceCard;
+
+    private ArrayList<Direction> PositionsFromBase;
+
+    private ArrayList<Kingdom> ResourceFromBase;
+
+    /**
+     * PositionGoalCard constructor
+     * @param Points given when the requirements are satisfied
+     * @param PositionsFromBase relative position of the cards from the lowest one on the game zone
+     * @param ResourceFromBase kingdom of the cards required, corresponding to the PositionFromBase ArrayList
+     * isResourceGoalCard is always false for this class
+     */
+    public PositionGoalCard (int Points, ArrayList<Direction> PositionsFromBase, ArrayList<Kingdom> ResourceFromBase) {
+        super(Points, false);
+        this.PositionsFromBase.addAll(PositionsFromBase);
+        this.ResourceFromBase.addAll(ResourceFromBase);
+    }
+
+    /**
+     * Points getter
+     * @return Points
+     */
+    @Override
+    public int getPoints() {
+        return Points;
+    }
+
+    /**
+     * isResourceGoal getter
+     * @return false
+     */
+    @Override
+    public boolean isResourceGoal() {
+        return super.isResourceGoal();
+    }
+
+    /**
+     * getPositionsFromBase getter
+     * @return getPositionsFromBase
+     */
+    public ArrayList<Direction> getPositionsFromBase() {
+        return PositionsFromBase;
+    }
+
+    /**
+     * getResourceFromBase getter
+     * @return getResourceFromBase
+     */
+    public ArrayList<Kingdom> getResourceFromBase() {
+        return ResourceFromBase;
+    }
 }
