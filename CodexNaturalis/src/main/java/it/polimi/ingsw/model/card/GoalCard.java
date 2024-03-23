0dc8630bf1959fed4b879e919ca74d972b1d7647
lorenzoMeroi22 <lorenzo.meroi@mail.polimi.it>
2024-03-23 10:37:47 +0100

    added GoalCard and ResourceGoalCard

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/GoalCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/GoalCard.java
@@ -1,4 +1,37 @@
 package it.polimi.ingsw.model.card;
 
+/**
+ * class GoalCard
+ * @author Lorenzo Meroi
+ */
 abstract public class GoalCard {
+    private int Points;
+
+    private boolean IsResourceGoal;
+
+    /**
+     * Constructor
+     * @param Points points given when the requirements are satisfied
+     * @param IsResourceGoal type of GoalCard
+     */
+    public GoalCard(int Points, boolean IsResourceGoal) {
+        this.Points = Points;
+        this.IsResourceGoal = IsResourceGoal;
+    }
+
+    /**
+     * Points getter
+     * @return Points
+     */
+    public int getPoints() {
+        return Points;
+    }
+
+    /**
+     * IsResourceGoal getter
+     * @return boolean describing the card type
+     */
+    public boolean isResourceGoal() {
+        return IsResourceGoal;
+    }
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/ResourceGoalCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/ResourceGoalCard.java
@@ -1,4 +1,52 @@
 package it.polimi.ingsw.model.card;
 
+import java.util.ArrayList;
+
+/**
+ * class ResourceGoalCard
+ * @author Lorenzo Meroi
+ */
 public class ResourceGoalCard extends GoalCard{
+    private int Points;
+
+    private boolean IsResourceGoal;
+
+    private ArrayList<Resource> Requirements;
+
+    /**
+     * ResourceGoalCard constructor
+     * @param Points given when the card is satisfied
+     * @param requirements ArrayList of the Resources required to satisfy the goal
+     * IsResourceGoal is always true for this class
+     */
+    public ResourceGoalCard (int Points, ArrayList<Resource> requirements) {
+        super(Points, true);
+        this.Requirements.addAll(requirements);
+    }
+
+    /**
+     * Points getter
+     * @return Points
+     */
+    @Override
+    public int getPoints() {
+        return Points;
+    }
+
+    /**
+     * isResourceGoal getter
+     * @return true
+     */
+    @Override
+    public boolean isResourceGoal() {
+        return IsResourceGoal;
+    }
+
+    /**
+     * Requirements getter
+     * @return Requirements
+     */
+    public ArrayList<Resource> getRequirements() {
+        return Requirements;
+    }
 }
