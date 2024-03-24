66be842f48d60ee02a2bc48a128f20764207b145
lorenzoMeroi22 <lorenzo.meroi@mail.polimi.it>
2024-03-24 12:08:27 +0100

    PlayerField class started

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/PlayerField.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/PlayerField.java
@@ -1,4 +1,171 @@
 package it.polimi.ingsw.model.gamelogic;
 
+import it.polimi.ingsw.model.card.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * PlayerField class
+ * @author Lorenzo Meroi
+ */
 public class PlayerField {
+    private ArrayList<ResourceCard> Hand;
+
+    private GoalCard PrivateGoal;
+
+    private Map<Coordinates, GameCard> GameZone;
+
+    private Map<Resource, Integer> ResourceMap;
+
+    /**
+     * PlayerField constructor
+     * @param startingCard the first card to be ever placed in the game zone
+     */
+    public PlayerField (StartingCard startingCard) {
+        Hand = new ArrayList<ResourceCard>();
+        GameZone = new HashMap<Coordinates, GameCard>();
+        GameZone.put(new Coordinates(0,0), startingCard);
+        ResourceMap = new HashMap<Resource, Integer>();
+
+        for (Resource resource : startingCard.getPermanentResources()) {
+            if (resource != null) {
+                ResourceMap.put(resource, ResourceMap.get(resource) + 1);
+            }
+        }
+        for (int i = 0; i<=3; i++) {
+            Resource resource = startingCard.getCorner(i);
+            if (resource!=null && !resource.equals(Resource.BLANK) && !resource.equals(Resource.HIDDEN)) {
+                ResourceMap.put(resource, ResourceMap.get(resource)+1);
+            }
+        }
+
+    }
+
+    /**
+     * Hand getter
+     * @return ArrayList<ResourceCard>
+     */
+    public ArrayList<ResourceCard> getHand() {
+        return Hand;
+    }
+
+    /**
+     * method to draw a card from a deck
+     * @param from which deck (Resource or Gold cards) to draw from
+     * @param which whether to draw from the covered cards in the deck or from the uncovered ones (0 for deck, 1 for the first uncovered, 2 for the second)
+     */
+    public void Draw (Deck from, int which) {
+        ResourceCard drawn = from.Draw(which);
+        Hand.add(drawn);
+    }
+
+    /**
+     * method to play a card in a position
+     * @param where the coordinates in which you want to play
+     * @param card the card you want to play
+     */
+    public int Play (Coordinates where, ResourceCard card) {
+        if (IsPlayable(where, card)) {
+            GameZone.put(where, card);
+            if (!card.getIsFront()) {
+                Resource permanentResource = getResource(card);
+
+                ResourceMap.put(permanentResource, ResourceMap.get(permanentResource)+1);
+            }
+            else {
+                for (int i=0; i<4; i++) {
+                    Resource resource = card.getCorner(i);
+                    if (resource!=null && !resource.equals(Resource.HIDDEN) && !resource.equals(Resource.BLANK) && !resource.equals(Resource.COVERED)) {
+                        ResourceMap.put(resource, ResourceMap.get(resource)+1);
+                    }
+                }
+            }
+            //devo togliere le risorse dagli angoli delle carte che ho coperto, ma non so come gestire le coordinate
+            return calculateCardPoints(where, card);
+        }
+        else{
+            System.out.println("This card cannot be played here");
+        }
+        return 0;
+    }
+
+    /**
+     * method to get the permanent Resource of a Resource/gold card (the one on the back, corresponding to its kingdom)
+     * @param card which card you want to get the resource from
+     * @return the permanent resource of that card
+     */
+    private static Resource getResource(ResourceCard card) {
+        Kingdom cardKingdom = card.getKingdom();
+        Resource permanentResource = null;
+        if (cardKingdom.equals(Kingdom.PLANT)) {
+            permanentResource = Resource.PLANT;
+        } else if (cardKingdom.equals(Kingdom.ANIMAL)) {
+            permanentResource = Resource.ANIMAL;
+        } else if (cardKingdom.equals(Kingdom.FUNGI)) {
+            permanentResource = Resource.FUNGI;
+        } else if (cardKingdom.equals(Kingdom.INSECT)) {
+            permanentResource = Resource.INSECT;
+        }
+        return permanentResource;
+    }
+
+    /**
+     * method to know if a card is playable in the given coordinates
+     * @param where the coordinates in which you want to play the card
+     * @param card the card you want to play
+     * @return true if it's playable, false if it is not
+     */
+    public boolean IsPlayable (Coordinates where, ResourceCard card) {}
+
+    /**
+     * method to calculate the points given when playing a card
+     * @param where the coordinates in which you want to play the card
+     * @param card the card you want to play
+     * @return the points you get from playing it
+     */
+    private int calculateCardPoints (Coordinates where, ResourceCard card) {}
+
+    /**
+     * private goal getter
+     * @return GoalCard
+     */
+    public GoalCard getPrivateGoal() {
+        return PrivateGoal;
+    }
+
+    /**
+     * private goal setter
+     * @param privateGoal
+     */
+    public void setPrivateGoal(GoalCard privateGoal) {
+        PrivateGoal = privateGoal;
+    }
+
+    /**
+     * game zone getter
+     * @return Map<Coordinates, GameCard>
+     */
+    public Map<Coordinates, GameCard> getGameZone() {
+        return GameZone;
+    }
+
+    /**
+     * resource from resource map getter
+     * @param which resource to get
+     * @return int
+     */
+    public int getResource (Resource which) {
+        return ResourceMap.get(which);
+    }
+
+    /**
+     * resource from resource map setter
+     * @param which resource to set
+     * @param how to set it
+     */
+    public void setResource(Resource which, int how) {
+        ResourceMap.put(which, how);
+    }
 }
