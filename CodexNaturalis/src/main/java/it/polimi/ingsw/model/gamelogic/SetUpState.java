9c3461b90f13c16c0f8f344050a64e779ece218b
lorenzoMeroi22 <lorenzo.meroi@mail.polimi.it>
2024-04-08 09:38:16 +0200

    handle method of SetUpState class started

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/SetUpState.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/SetUpState.java
@@ -22,7 +22,26 @@ public class SetUpState extends State{
      */
     @Override
     public void HandleState() {
+        //temporarily I assume there's going to be 4 players
+        ArrayList<Player> Players = new ArrayList<Player>();
+        Players.add(new Player("Lorenzo", Color.RED));
+        Players.add(new Player("Gabriel", Color.BLUE));
+        Players.add(new Player("Matteo", Color.GREEN));
+        Players.add(new Player("Francesk", Color.YELLOW));
+        //end of Players initialization
 
+        ScoreBoard scoreBoard = new ScoreBoard(Players);
+        Deck goldDeck = new Deck(true);
+        Deck resurceDeck = new Deck(false);
+
+        ArrayList<PlayerField> playerFields = new ArrayList<>();
+        //@TODO parse starting cards and give one to a random playerfield
+
+        //@TODO parse goal cards, first give one to each playerfield through the PlayerField.setPrivateGoal method; then choose two of them as the common goals
+        // put them into an array for the GameTable constructor
+        //GameTable gameTable = new GameTable();
+        GameState gameState = new GameState(Players, Players.get(0), gameTable);
+        this.game = gameState;
     }
 
     /**
