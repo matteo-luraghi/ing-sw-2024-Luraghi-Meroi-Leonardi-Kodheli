c44d1d1254cff96b3631dfb9c87d65688fca3ccd
GabrielLeonardiPoli <gabriel.leonardi@mail.polimi.it>
2024-04-18 13:47:10 +0200

    Refactored controller package

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller/MegaController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/MegaController.java
@@ -1,6 +1,9 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.model.gamelogic.Color;
 import it.polimi.ingsw.model.gamelogic.GameState;
+import it.polimi.ingsw.model.gamelogic.Player;
+import it.polimi.ingsw.model.gamelogic.Util;
 
 import java.util.Scanner;
 
@@ -10,7 +13,6 @@ import java.util.Scanner;
  */
 public class MegaController {
     private GameState game;
-    private ControllerState controllerState;
     protected Scanner scanner;
     private int numOfPlayers;
     /**
@@ -19,16 +21,9 @@ public class MegaController {
      */
     public MegaController(GameState game){
         this.game = game;
-        this.controllerState = new LoginPlayer();
         this.scanner = new Scanner(System.in);
         numOfPlayers = -1;
     }
-
-    /**
-     * ControllerState setter, making it possible to transition between states
-     * @param newState the controllerState i want to switch to
-     */
-    public void setState(ControllerState newState){ this.controllerState = newState; }
 
     /**
      * gameState getter
@@ -52,5 +47,82 @@ public class MegaController {
      */
     public void setNumOfPlayers(int numOfPlayers) {
         this.numOfPlayers = numOfPlayers;
+    }
+
+    private int giveNumberPlayers(){
+        int num;
+        if(numOfPlayers == -1){
+            //TODO: view the message "You are the first player"
+            do{
+                //TODO: view the message "Choose the number of players for this game"
+                num = scanner.nextInt();
+                if(num < 2 || num > 4){
+                    //TODO: view the message "Invalid number of players, choose again"
+                }
+            }while(num < 2 || num > 4);
+            return num;
+        } else {
+            System.err.println("You aren't the first player, you cannot set how many players there are, returning -1");
+            return -1;
+        }
+    }
+
+    /**
+     * Method to let a user register himself as a player
+     * @return the player object constructed by the user
+     */
+    private Player setPlayer(){
+        String nick;
+        String colorString;
+        boolean correct;
+        Color color = null; //Will always get initialized in the second do-while loop
+
+        //Ask the player for his username
+        do {
+            nick = scanner.nextLine();
+            correct = checkUniqueNickname(nick);
+            if(!correct){
+                controller.view.showMessage("Username already exists, please choose another username");
+            }
+        }while (!correct);
+
+        //Ask the player for his chosen color
+        do {
+            colorString = scanner.nextLine();
+            try{
+                color = Util.stringToColor(colorString);
+                correct = checkUniqueColor(color);
+                if(!correct) controller.view.showMessage("Color is already taken, please choose a free color");
+            } catch (NullPointerException e){
+                controller.view.showMessage("Color doesn't exist, please choose a valid color");
+                correct = false;
+            }
+        }while (!correct);
+
+        return new Player(nick, color);
+    }
+
+    /**
+     * Method that checks if a player nickname is already present in the game
+     * @param nick the nickname that needs checking
+     * @return true if it's NOT already present, false if it is
+     */
+    private boolean checkUniqueNickname(String nick){
+        for(Player p : getGame().getPlayers()){
+            if(p.getNickname().equals(nick)) return false;
+        }
+        return true;
+    }
+
+    /**
+     * Method that checks if a player chosen color is already present in the game
+     * @param color the color that needs checking
+     * @return true if it's NOT already present, false if it is
+     */
+    private boolean checkUniqueColor(Color color){
+        for(Player p : getGame().getPlayers()){
+            if(p.getColor() == color) return false;
+        }
+        return true;
     }
 }
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/ViewObserver.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.controller;
+
+/**
+ * View Observer, observes the view for game flow changes and updates the MegaController
+ * @author Gabriel Leonardi
+ */
+public class ViewObserver {
+    public void publish(String message){
+        System.err.println("ViewObserver not yet implemented, message sent: " + message);
+    }
+}
