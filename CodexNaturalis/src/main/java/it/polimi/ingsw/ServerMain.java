4803a3ef8ecd8e447e77f8eb2c20e52962fea5ed
matteoleonardoluraghi02 <matteoleonardo.luraghi@mail.polimi.it>
2024-04-23 16:14:45 +0200

    trying to initialize the server with RMI

--- CodexNaturalis/src/main/java/it/polimi/ingsw/ServerMain.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/ServerMain.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.controller.MegaController;
+import it.polimi.ingsw.model.gamelogic.GameState;
 
 import java.net.MalformedURLException;
 import java.rmi.Naming;
@@ -8,11 +10,19 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 
+/**
+ * Deck class
+ * @author Matteo Leonardo Luraghi
+ */
 public class ServerMain implements Remote {
-    public void start() throws RemoteException {
+    public static void main(String[] args) {
+        //TODO: start the server and idle for games
+    }
+    public void startGame() throws RemoteException {
         LocateRegistry.createRegistry(1099);
         // esportazione dell'oggetto, tutte le modifiche sulla copia devono essere trasferite all'oggetto originale
-        Remote stub = (Remote) UnicastRemoteObject.exportObject(obj, 0);
+        MegaController controller = new MegaController(new GameState());
+        Remote stub = (Remote) UnicastRemoteObject.exportObject(controller, 0);
         try {
             Naming.rebind("//localhost/Server", stub);
         } catch (MalformedURLException e) {
