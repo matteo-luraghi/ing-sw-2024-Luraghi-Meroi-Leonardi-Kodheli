6781e542db28a169c617e3c8fd599c3b51129c5e
lorenzoMeroi22 <lorenzo.meroi@mail.polimi.it>
2024-04-25 15:54:06 +0200

    Finished ViewGoalCardCLIFactory class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/cli/ViewGoalCardCLIFactory.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/cli/ViewGoalCardCLIFactory.java
@@ -1,9 +1,7 @@
 package it.polimi.ingsw.view.cli;
 
-import it.polimi.ingsw.model.card.Direction;
-import it.polimi.ingsw.model.card.PositionGoalCard;
-import it.polimi.ingsw.model.card.Resource;
-import it.polimi.ingsw.model.card.ResourceGoalCard;
+import it.polimi.ingsw.model.card.*;
+import it.polimi.ingsw.view.mainview.AnsiColors;
 import it.polimi.ingsw.view.mainview.ViewGoalCardFactory;
 
 import java.util.ArrayList;
@@ -25,8 +23,14 @@ public class ViewGoalCardCLIFactory extends ViewGoalCardFactory {
 
          _____2_____
         |   I       |
-        |     I     |  Stair-type goal card
+        |     I     |  left Stair-type goal card
         |       I   |
+        |___________|
+
+         _____2_____
+        |       I   |
+        |     I     |  right Stair-type goal card
+        |   I       |
         |___________|
 
          _____2_____
@@ -45,19 +49,67 @@ public class ViewGoalCardCLIFactory extends ViewGoalCardFactory {
         if (this.card.isResourceGoal()) {
             //TODO show a resource goal card
             ArrayList<Resource> requirements = ((ResourceGoalCard) this.card).getRequirements();
-            if (requirements.get(0).equals(Resource.FEATHER) || requirements.get(0).equals(Resource.SCROLL) || requirements.get(0).equals(Resource.POTION)) {
-                //It is a Gold Resource-type goal card
-            } else {
-                //it is a Natural Resource-type goal card
+
+            System.out.print(AnsiColors.ANSI_WHITE);
+            System.out.println(" _____"+this.card.getPoints()+"_____ ");
+            System.out.println("|           |");
+            switch (requirements.size()) {
+                case 2 -> {System.out.println("|    "+requirements.get(0).toString()+AnsiColors.ANSI_WHITE+" "+requirements.get(1).toString()+AnsiColors.ANSI_WHITE+"    |");}
+                case 3 -> {System.out.println("|   "+requirements.get(0).toString()+AnsiColors.ANSI_WHITE+" "+requirements.get(1).toString()+AnsiColors.ANSI_WHITE+" "+requirements.get(2).toString()+AnsiColors.ANSI_WHITE+"   |");}
             }
+            System.out.println("|           |");
+            System.out.println("|___________|");
+            System.out.print(AnsiColors.ANSI_RESET);
         } else {
             //TODO show a position goal card
             ArrayList<Direction> directions = ((PositionGoalCard) this.card).getPositionsFromBase();
+            ArrayList<Kingdom> resources = ((PositionGoalCard) this.card).getResourceFromBase();
+
+            System.out.print(AnsiColors.ANSI_WHITE);
+            System.out.println(" _____"+this.card.getPoints()+"_____ ");
+
             if (directions.contains(Direction.TOP)) {
                 //It is an L-type goal card
+                if (directions.get(0).equals(Direction.TOP)) {
+                    //It is an upside down L
+                    if (directions.get(1).equals(Direction.TOP_RIGHT)) {
+                        //it is facing eastward
+                        System.out.println("|   " + resources.get(2) + AnsiColors.ANSI_WHITE + "       |");
+                    } else {
+                        //It is facing westward
+                        System.out.println("|       " + resources.get(2) + AnsiColors.ANSI_WHITE + "   |");
+                    }
+                    System.out.println("|     " + resources.get(1) + AnsiColors.ANSI_WHITE + "     |");
+                    System.out.println("|     " + resources.get(0) + AnsiColors.ANSI_WHITE + "     |");
+                } else {
+                    //It is an L
+                    System.out.println("|     " + resources.get(2) + AnsiColors.ANSI_WHITE + "     |");
+                    System.out.println("|     " + resources.get(1) + AnsiColors.ANSI_WHITE + "     |");
+                    if (directions.get(0).equals(Direction.TOP_RIGHT)) {
+                        //it is facing westward
+                        System.out.println("|   " + resources.get(0) + AnsiColors.ANSI_WHITE + "       |");
+                    } else {
+                        //it is a normal L, facing eastward
+                        System.out.println("|       " + resources.get(0) + AnsiColors.ANSI_WHITE + "   |");
+                    }
+                }
             } else {
                 //It is a stair-type goal card
+                if (directions.contains(Direction.TOP_RIGHT)) {
+                    //it is a right stair
+                    System.out.println("|       "+resources.get(2)+AnsiColors.ANSI_WHITE+"   |");
+                    System.out.println("|     "+resources.get(1)+AnsiColors.ANSI_WHITE+"     |");
+                    System.out.println("|   "+resources.get(0)+AnsiColors.ANSI_WHITE+"       |");
+                } else {
+                    //it is a left stair
+                    System.out.println("|   "+resources.get(2)+AnsiColors.ANSI_WHITE+"       |");
+                    System.out.println("|     "+resources.get(1)+AnsiColors.ANSI_WHITE+"     |");
+                    System.out.println("|       "+resources.get(0)+AnsiColors.ANSI_WHITE+"   |");
+                }
             }
+
+            System.out.println("|___________|");
+            System.out.print(AnsiColors.ANSI_RESET);
         }
 
     }
