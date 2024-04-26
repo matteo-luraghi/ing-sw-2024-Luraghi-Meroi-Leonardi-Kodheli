7a558c2c1188a8b92805dc499526f4461a833661
Françesko <francesk.kodheli@mail.polimi.it>
2024-04-26 21:12:08 +0200

    Fixed path, added exception handling on draw

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/Deck.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/Deck.java
@@ -4,14 +4,11 @@ import com.google.gson.Gson;
 import it.polimi.ingsw.model.card.GoldCard;
 import it.polimi.ingsw.model.card.ResourceCard;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Collections;
+import java.util.*;
 
 /**
  * Deck class
@@ -37,7 +34,11 @@ public class Deck {
 
         if(!isGold){
             for(int i=1; i<=40; i++) {
-                String cardPath = "../resources/CardsJSON/resourceCards/resourceCard" + i + ".json"; //TODO: fix root path
+
+
+                String cardPath = "./CodexNaturalis/src/main/resources/CardsJSON/resourceCards/resourceCard" + i + ".json";
+                //fixed path
+
                 // initialize the json file reader and save the card
                 try(Reader reader = new FileReader(cardPath)) {
                     ResourceCard card = gson.fromJson(reader, ResourceCard.class);
@@ -48,7 +49,8 @@ public class Deck {
             }
         } else {
             for(int i=1; i<=40; i++) {
-                String cardPath = "../resources/CardsJSON/goldCards/goldCard" + i + ".json"; //TODO: fix root path
+                String cardPath = "./CodexNaturalis/src/main/resources/CardsJSON/resourceCards/resourceCard" + i + ".json";
+                //fixed path
                 // initialize the json file reader and save the card
                 try(Reader reader = new FileReader(cardPath)) {
                     GoldCard card = gson.fromJson(reader, GoldCard.class);
@@ -97,10 +99,19 @@ public class Deck {
      */
     private ResourceCard Draw (int which)  {
         if (which == 0) {//drawing from deck
-            return cards.remove();
+            try {
+
+
+                return cards.remove();
+            }
+            catch (NoSuchElementException e) //added exception in case the deck is empty and the call is made from the  uncovered method
+            {
+                System.err.println("Empty Deck");
+            }
         } else { //drawing from uncovered cards
             try {
                 ResourceCard drawn = uncoveredCards[which-1];
+                if(drawn!=null) //check in case there are no covered, neither covered cards
                 setUncoveredCard(which);
                 return drawn;
             } catch (NullPointerException e) {  //TODO: Custom exception?
@@ -148,7 +159,8 @@ public class Deck {
      * @param Which can be 1(first uncovered card) or 2(second uncovered card)
      */
     public void setUncoveredCard (int Which) {
-        this.uncoveredCards[Which-1] = this.Draw(0);
+
+        uncoveredCards[Which-1] = Draw(0);
     }
 
     public boolean isDeckEmpty () {
