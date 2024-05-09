d5e85996d88a157a87a79cf4216af6b0663fbb6a
GabrielLeonardiPoli <gabriel.leonardi@mail.polimi.it>
2024-05-09 13:05:27 +0200

    Fixed RMI error

--- CodexNaturalis/src/main/java/module-info.java
+++ CodexNaturalis/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module it.polimi.ingsw.psp17 {
 
     requires org.controlsfx.controls;
     requires com.google.gson;
+    requires java.rmi;
 
     opens it.polimi.ingsw.psp17 to javafx.fxml;
     exports it.polimi.ingsw.psp17;
