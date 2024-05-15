68de1ccb4bce0ecb677d93a9adba912d7c6b2df1
GabrielLeonardiPoli <gabriel.leonardi@mail.polimi.it>
2024-05-15 11:04:15 +0200

    Added controller to Login page and Join Game page

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/gui/JoinGameController.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.view.gui;
+
+public class JoinGameController {
+    private GUI view;
+
+    public void setView(GUI view){
+        this.view = view;
+    }
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/gui/LoginController.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.view.gui;
+
+public class LoginController {
+    private GUI view;
+
+    public void setView(GUI view){
+        this.view = view;
+    }
+}
