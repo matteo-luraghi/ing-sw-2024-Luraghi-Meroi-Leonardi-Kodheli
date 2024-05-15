b59187b2a4578762810f771fac8a596477dba604
GabrielLeonardiPoli <gabriel.leonardi@mail.polimi.it>
2024-05-15 17:59:04 +0200

    Created Setup and Player field event handler stubs

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/gui/eventhandlers/SetupController.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.view.gui.eventhandlers;
+
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.ImageView;
+
+public class SetupController extends EventHandler{
+    public ImageView startingImageFront;
+    public ImageView startingImageBack;
+    public RadioButton startingFront;
+    public RadioButton startingBack;
+    public ImageView goalImage1;
+    public ImageView goalImage2;
+    public RadioButton goal1;
+    public RadioButton goal2;
+
+    public void initialize(){
+
+    }
+}
