37794d07e8232bd052211e7f09b8e4f81cdc2616
FranceskKodheli1 <francesk.kodheli@mail.polimi.it>
2024-06-23 21:35:45 +0200

    Color Class fixed Javadoc

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/Color.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/Color.java
@@ -18,6 +18,10 @@ public enum Color implements Serializable {
     @Serial
     private static final long serialVersionUID = -3997072925040508193L;
 
+    /**
+     * Convert the current color to string
+     * @return string format of the color otherwise "UNKNOWN" string
+     */
     @Override
     public String toString() {
         switch (this) {
@@ -28,6 +32,11 @@ public enum Color implements Serializable {
             default -> {return "UNKNOWN";}
         }
     }
+
+    /**
+     * Implementation for gui, since can't use properly AnsiColors
+     * @return String format of the color if present in the enum otherwise "UNKNOWN"
+     */
     public String toStringGUI(){
         switch (this) {
             case YELLOW -> { return "yellow";}
